ab61dc0b9246588a763b51a268cc7c8c2dc03e41
Andy Jiahui Z <zjarsen@gmail.com>
2015-09-10 11:17:48 +0200

    final (without data send)

--- cppdemo/eyeTracking-offscreenCalibration.cpp
+++ cppdemo/eyeTracking-offscreenCalibration.cpp
@@ -10,6 +10,7 @@
 //added
 #include <string.h>
 #include <string>
+#include <thread>
 #define PORTNUM 12355
 #define BUFMAX 1024
 //end
@@ -56,11 +57,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		//change the address below to your connected ROS computer address
 		serverAddr.sin_addr.s_addr = inet_addr("192.168.15.49");
 
-		sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-
-		//windowsEchoLoop(sock, (SOCKADDR*)& serverAddr, sizeof(serverAddr));
-		
-		
+		sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);		
 	}
 	else
 	{
@@ -71,7 +68,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	int ret_calibrate = 0, ret_validate = 0, ret_connect = 0, ret_acceptCalibrationPoint; 
 	char c = ' ';
 	char repeat = ' ';
-	int isSuccessful = 0;
 	char r = ' ';
 
 	std::cout << "Output of off-screen calibration"<< std::endl;
@@ -95,112 +91,63 @@ int _tmain(int argc, _TCHAR* argv[])
 			return 0;
 	}
 
-	if(ret_connect == RET_SUCCESS)
+	if (ret_connect == RET_SUCCESS)
 	{
 		// Ready to perform a calibration?
- 		std::cout <<  "Press Enter to calibrate" << std::endl;
-		getchar(); 
- 
- 		if(1)
+		std::cout << "Press Enter to calibrate" << std::endl;
+		getchar();
+
+		// set up calibration point
+		iV_SetSpeedMode(30);
+		iV_ChangeCalibrationPoint(1, 20, 35);
+		iV_ChangeCalibrationPoint(2, 543, 516);
+		iV_ChangeCalibrationPoint(3, 891, 711);
+		iV_ChangeCalibrationPoint(4, 983, 52);
+		iV_ChangeCalibrationPoint(5, 1231, 225);
+
+		// set up calibration
+		calibrationData.method = 5;
+		calibrationData.speed = 0;
+		calibrationData.displayDevice = 0;
+		calibrationData.targetShape = 1;
+		calibrationData.foregroundBrightness = 250;
+		calibrationData.backgroundBrightness = 60;
+		calibrationData.autoAccept = 2;
+		calibrationData.targetSize = 80;
+		calibrationData.visualization = 1;
+		strcpy_s(calibrationData.targetFilename, 256, "");
+
+		iV_SetupCalibration(&calibrationData);
+
+		// start calibration
+		ret_calibrate = iV_Calibrate();
+
+		switch (ret_calibrate)
 		{
-			// set up calibration point
-			iV_SetSpeedMode(30);
-			iV_ChangeCalibrationPoint(1, 20, 35);
-			iV_ChangeCalibrationPoint(2, 543, 516);
-			iV_ChangeCalibrationPoint(3, 891, 711);
-			iV_ChangeCalibrationPoint(4, 983, 52);
-			iV_ChangeCalibrationPoint(5, 1231, 225);
-
-			// set up calibration
-			calibrationData.method = 5;
-			calibrationData.speed = 0;
-			calibrationData.displayDevice = 0;
-			calibrationData.targetShape = 1;
-			calibrationData.foregroundBrightness = 250;
-			calibrationData.backgroundBrightness = 60;
-			calibrationData.autoAccept = 2;
-			calibrationData.targetSize = 80;
-			calibrationData.visualization = 1;
-			strcpy_s(calibrationData.targetFilename, 256, "");
-
-			iV_SetupCalibration(&calibrationData);
-
-			//loop for calibration (successful calibration can skip the loop)
-			while(isSuccessful == 0){
-				
-				// start calibration
-				ret_calibrate = iV_Calibrate();
-
-				switch (ret_calibrate)
-				{
-				case RET_SUCCESS:
-					std::cout << "Validation starts" << std::endl;
-
-					// start validation 
-					ret_validate = iV_Validate();
-
-					// show accuracy only if validation was successful
-					if (ret_validate == RET_SUCCESS)
-					{
-						std::cout << "iV_GetAccuracy: " << iV_GetAccuracy(&accuracyData, 0) << std::endl;
-						std::cout << "AccuracyData DevX: " << accuracyData.deviationLX << " DevY: " << accuracyData.deviationLY << std::endl;
-						
-						//the statement below is applied to the situation of successful projection point setup. When the deviation is too high, the calibration fails. make it a comment when the setup is not ready. conact Andy using zhuj0017@e.ntu.edu.sg for help
-						//if ((accuracyData.deviationLX <= 0.8) && (accuracyData.deviationLY) <= 0.8) {
-						if(1){
-							isSuccessful = 1;
-							std::cout << "Calibration is successful. Press Enter to start tracking" << std::endl;
-						}
-						else {
-							std::cout << "the accuracy is too low ";
-						}
-						getchar();
-					}
-					break;
-				case ERR_NOT_CONNECTED:
-					std::cout << "iViewX is not reachable" << std::endl;
-					break;
-				case ERR_WRONG_PARAMETER:
-					std::cout << "Wrong Parameter used" << std::endl;
-					break;
-				case ERR_WRONG_DEVICE:
-					std::cout << "Not possible to calibrate connected Eye Tracking System" << std::endl;
-					break;
-				default:
-					std::cout << "Calibration quited" << std::endl;
-					r = 'q';
-					getchar();
-					break;
-				}
-
-				if (r == 'q') {
-					break;
-				}
-
-				if (isSuccessful == 0) {
-					//when the calibration failed: shall we recalibrate?
-					std::cout << "The calibration failed. Key in any value to recalibrate, or press q to quit the calibration." << std::endl;
-					r = getchar();
-					//flush the buffer
-					getchar();
-					//if yes, continue the loop. if no, end of the program.
-					if (r == 'q') {
-						break;
-					} 
-				}
-
-			}
+		case RET_SUCCESS:
+			break;
+		case ERR_NOT_CONNECTED:
+			std::cout << "iViewX is not reachable" << std::endl;
+			break;
+		case ERR_WRONG_PARAMETER:
+			std::cout << "Wrong Parameter used" << std::endl;
+			break;
+		case ERR_WRONG_DEVICE:
+			std::cout << "Not possible to calibrate connected Eye Tracking System" << std::endl;
+			break;
+		default:
+			std::cout << "Calibration quited" << std::endl;
+			getchar();
+			break;
 		}
 
-		if (r == 'q') {
-			return 0;
-		}
+	}
 
 		// start data output via callback function
 		// define a callback function for receiving samples. When you want the eye data to be sent to ROS, comment the statement below, or the data will be shown on the NUC screen
-		//iV_SetSampleCallback(SampleCallbackFunction);
-
-		
+	std::cout << "press enter to start eye tracking" << std::endl;
+	getchar();
+			
 		while (1) {
 			//add windowsEchoLoop
 			int bytesRead;
@@ -232,19 +179,17 @@ int _tmain(int argc, _TCHAR* argv[])
 				//exit(sendToResult);
 				//}
 			//end
-		}				
+		}
 
-		getchar();
+	getchar();
 		
-		// disable callback
-		iV_SetSampleCallback(NULL);
+	// disable callback
+	iV_SetSampleCallback(NULL);
 
-		// disconnect
-		std::cout << "iV_Disconnect: " << iV_Disconnect() << std::endl;
+	// disconnect
+	std::cout << "iV_Disconnect: " << iV_Disconnect() << std::endl;
 
-		getchar();
-
-	}
+	getchar();
 
 	return 0;
 }
